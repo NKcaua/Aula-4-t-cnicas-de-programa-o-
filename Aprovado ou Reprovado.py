28d9796734a2ff01b30857760c1ce117be04bbb9
NKcaua <163947899+NKcaua@users.noreply.github.com>
2025-08-27 21:34:00 -0400

    Add files via upload

---
+++ Aprovado ou Reprovado.py
@@ -0,0 +1,6 @@
+nota = float(input("Digite a sua nota:\n "))
+
+if nota >= 7:
+    print("APROVADO!")
+else:
+    print("REPROVADO.")
---
+++ Comparador de Números.py
@@ -0,0 +1,14 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número (diferente do primeiro): "))
+
+if num1 == num2:
+    print("Os numeros são iguais, por favor reinicia e coloque numeros diferentes. ")
+else:
+    if num1 > num2:
+
+    print(f"O maior numero é:"{num1})
+
+else:
+
+    print(f"O maior numero é:"{num2})
+
